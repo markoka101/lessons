f5471bb4167bebd019d06059d439ebbf4d7ccefa
markoka101 <markoka101@gmail.com>
2023-09-25 14:37:03 -0400

    removed boiler plate code from demo

--- L1/demo.js
+++ L1/demo.js
@@ -2,8 +2,4 @@
 This file will be used for demo before working on concepts
 */
 
-//boiler plate code
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
- });
+
