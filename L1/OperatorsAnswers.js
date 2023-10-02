7172f2d0a443846f19cb4be39943ff9768cf6f51
markoka101 <markoka101@gmail.com>
2023-10-02 15:41:06 -0400

    Explained my solutions in the Operations answer file

--- L1/OperatorsAnswers.js
+++ L1/OperatorsAnswers.js
@@ -25,6 +25,7 @@ Basic logic operators
 This function should return true if even and false if odd
 */
 function isEven(x) {
+    //This returns the boolean value of the expression
     return (x % 2 === 0);
 }
 
@@ -34,6 +35,9 @@ The operators will be addition '+' , subtraction '-' , division '/' , and multip
 From there you will return the result of the two numbers with the selected operator
 */
 function calculate(x,y,operator) {
+    //I used a swith statement instead of if - else for readability and speed reasons
+    //Since we are just checking for equality of a single character it is faster to use switch
+    //From there I just return the value of the equation
     switch(operator) {
         case '+':
             return (x+y);
@@ -51,9 +55,14 @@ This function will return the summation of numbers from 1 to x
 Use a for loop to get sum
 */
 function summation(x) {
+    //I declared sum out of the loop and set it to zero
+    //If i declared it inside the loop, it would reset for every iteration
     let sum = 0;
 
+    //runs from 1 until the number given
+    //I used <= since it will run until it is equal unlike <
     for(let i = 1; i <= x; i++) {
+        //adds the index to the current sum
         sum += i;
     }
     return sum;
@@ -72,6 +81,7 @@ function fizzbuzz(n) {
 
     //create the for loop
     for (let i = 1; i < n; i++) {
+        //15 is the least common multiple of 5 and 3
         if (i % 15 === 0) {
             result.push('fizzbuzz');
         } else if (i % 5 === 0) {
