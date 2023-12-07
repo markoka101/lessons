4d7c54fc4733b7aae4dca6258e73789e5bb38bad
markoka101 <markoka101@gmail.com>
2023-12-07 16:35:05 -0500

    Added folder for extra work with file for a class assignmenet

---
+++ ExtraWork/ClassesV2.js
@@ -0,0 +1,42 @@
+/*
+This object game consists of 3 different keys genre, price, and hasStory
+The <key,value> pairs are <genre, shooter>, <price, 40>, and <hasStory, true>
+*/
+const game  = {
+    //this is a string
+    genre : 'shooter',
+    //this is a number
+    price : 40,
+    //this is a bool
+    hasStory : true
+}
+
+/*
+This function should loop through the keys and print:
+"key is value"
+*/
+function printGame() {}
+
+
+/*
+Complete the class Cat
+The properties will be name, color, age, and vetBillTotal
+*/
+class Cat {
+
+    //This function should increment the age by 1
+    age(){}
+
+    //this function will take the parameter price and add it to vetBillTotal
+    addVetBill(price){}
+}
+
+/*
+Complete the MaineCoon class
+Add two properties meowsPerHour and weight
+*/
+class MaineCoon extends Cat {
+
+    //This will change the weight of the cat by the paraneter change
+    weightChange(change){}
+}
